d793a447a35b174256080ba85b394dc53dc5d390
Anubhav Tewari <58473176+anubhavtewari05@users.noreply.github.com>
2024-03-02 00:41:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,33 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+import bz2file as bz2
+import time
+
+def decompress_pickle(file):
+    model = bz2.BZ2File(file, 'rb')
+    model = pickle.load(model)
+    return model
+
 
 def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8fe5ff2c3388fae54605fe610fb5c1f8'.format(movie_id))
-    data = response.json()
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+    retries = 3
+    delay = 3  # Delay in seconds before retrying
+
+    for _ in range(retries):
+        try:
+            response = requests.get(
+                'https://api.themoviedb.org/3/movie/{}?api_key=8fe5ff2c3388fae54605fe610fb5c1f8'.format(movie_id))
+            if response.status_code == 200:
+                data = response.json()
+                poster_url = "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+                return poster_url
+            else:
+                time.sleep(delay)
+        except requests.ConnectionError as e:
+            time.sleep(delay)
+
+    return None
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
@@ -18,34 +40,42 @@ def recommend(movie):
 
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movies.append(movies.iloc[i[0]].title)
-        recommended_movies_posters.append(fetch_poster(movie_id))
-    return recommended_movies,recommended_movies_posters
+        poster = fetch_poster(movie_id)
+        if poster is not None:  # Check if poster is not None
+            recommended_movies.append(movies.iloc[i[0]].title)
+            recommended_movies_posters.append(poster)
+
+    return recommended_movies, recommended_movies_posters
 
 movies_list = pickle.load(open('movies_dict.pkl','rb'))
 movies = pd.DataFrame(movies_list)
-similarity = pickle.load(open('similarity,pkl','rb'))
+similarity = decompress_pickle('similarity.pbz2')
 
 st.title('Movie Recommender System')
-option_selected  = st.selectbox('Which movie do you like?',
-                       (movies['title'].values))
+option_selected  = st.selectbox('Which movie do you like?',(movies['title'].values))
 if st.button('Recommend'):
-    names,posters=recommend(option_selected)
+    names,posters = recommend(option_selected)
 
-    col1,col2,col3,col4,col5 = st.columns(5)
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
-
+    col1, col2, col3, col4, col5 = st.columns(5)
+    if names:
+        with col1:
+            st.text(names[0])  # Check if names list is not empty before accessing its elements
+            st.image(posters[0])
+        if len(names) > 1:
+            with col2:
+                st.text(names[1])
+                st.image(posters[1])
+        if len(names) > 2:
+            with col3:
+                st.text(names[2])
+                st.image(posters[2])
+        if len(names) > 3:
+            with col4:
+                st.text(names[3])
+                st.image(posters[3])
+        if len(names) > 4:
+            with col5:
+                st.text(names[4])
+                st.image(posters[4])
+    else:
+        st.error("No recommendations found for this movie.")
